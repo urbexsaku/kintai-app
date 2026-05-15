15fc084980593a462088293bfe6edcddfe309603
saaaaku <saaaaku@gmail.com>
2026-05-15 20:36:55 +0900

    implement: stamp.blade

--- src/app/Http/Controllers/StaffAttendanceController.php
+++ src/app/Http/Controllers/StaffAttendanceController.php
@@ -3,11 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\AttendanceRecord;
+use App\Models\BreakRecord;
 
 class StaffAttendanceController extends Controller
 {
     public function index()
     {
-        return view('staff.attendance.stamp');
+        // 今日の出勤データを取得
+        $attendance = AttendanceRecord::where('user_id', Auth::id())
+            ->whereDate('clock_in', today())
+            ->first();
+        
+        $break = null;
+
+        // 今日の出勤データが存在する場合、その勤怠に紐づく最新の休憩データを取得
+        if ($attendance) {
+            $break = BreakRecord::where('attendance_record_id', $attendance->id)
+                ->latest()
+                ->first();
+        }
+        
+        $isWorking = false;
+        $isBreaking = false;
+        $isFinished = false;
+
+        if ($attendance) {
+            if ($attendance->clock_out) { // 最新の勤怠データがclock_out
+                $isFinished = true; // 退勤済
+            } elseif ($break && !$break->end_at){ // 最新の休憩データがend_atではない場合
+                $isBreaking = true; // 休憩中
+            } else {
+                $isWorking = true; // 勤務中
+            }
+        }
+    
+        return view('staff.attendance.stamp', compact(
+            'attendance',
+            'isWorking',
+            'isBreaking',
+            'isFinished'
+        ));
     }
 }
--- src/resources/views/staff/attendance/stamp.blade.php
+++ src/resources/views/staff/attendance/stamp.blade.php
@@ -7,7 +7,48 @@
 @endsection
 
 @section('content')
+<div class="attendance">
+  <div class="attendance-status">
+    @if(!$attendance)<p>勤務外</p>
+    @elseif($isWordking)<p>出勤中</p>
+    @elseif($isBreaking)<p>休憩中</p>
+    @elseif($isFinished)<p>退勤済</p>
+  </div>
+  <div id="current-time"></div>
 
+  {{-- 出勤前 --}}
+  @if(!$attendance)
 
+  <form action="/attendance/clock-in" method="post">
+    @csrf
+    <button type="submit">出勤</button>
+  </form>
 
+  {{-- 出勤中 --}}
+  @elseif($isWordking)
+  <div class="attendance-button">
+    <form action="/attendance/clock-out" method="post">
+      @csrf
+      <button type="submit">退勤</button>
+    </form>
+
+    <form action="/attendance/break-start" method="post">
+      @csrf
+      <button type="submit">休憩入</button>
+    </form>
+  </div>
+
+  {{-- 休憩中 --}}
+  @elseif($isBreaking)
+  <form action="/attendance/break-end" method="post">
+    @csrf
+    <button type="submit">休憩戻</button>
+  </form>
+
+  {{-- 退勤後 --}}
+  @elseif($isFinished)
+  <p>お疲れさまでした。</p>
+  @endif
+</div>
+<script src="{{ asset('js/stamp.js') }}"></script>
 @endsection
