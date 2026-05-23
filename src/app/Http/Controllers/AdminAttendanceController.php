4deae68812ee51c7641abb66794e9efdca6d16c7
saaaaku <saaaaku@gmail.com>
2026-05-23 20:30:05 +0900

    作成: 管理者勤怠一覧画面

--- src/app/Http/Controllers/AdminAttendanceController.php
+++ src/app/Http/Controllers/AdminAttendanceController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\User;
 
 class AdminAttendanceController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.attendance.daily');
+        $date = $request->query('date', Carbon::today()->toDateString());
+
+        $currentDate = Carbon::parse($date);
+        $previousDate = $currentDate->copy()->subDay()->toDateString();
+        $nextDate = $currentDate->copy()->addDay()->toDateString();
+
+        $users = User::where('admin_status', false)
+            ->with([
+                'attendanceRecords' => function ($query) use ($currentDate) {
+                    $query->with('breakRecords')
+                        ->whereDate('work_date', $currentDate);
+                }
+            ])
+            ->get();
+
+        $users->each(function ($user) {
+            $user->setRelation(
+                'attendance',
+                $user->attendanceRecords->first()
+            );
+        });
+    
+        return view('admin.attendance.daily', compact(
+            'currentDate',
+            'previousDate',
+            'nextDate',
+            'users',
+        ));
     }
 }
