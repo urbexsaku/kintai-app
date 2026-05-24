470e00c31b1d4e43fa7b2de914cbf9eb1cb0cd62
saaaaku <saaaaku@gmail.com>
2026-05-24 17:27:42 +0900

    作成：スタッフ一覧画面

--- src/app/Http/Controllers/AdminStaffController.php
+++ src/app/Http/Controllers/AdminStaffController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminStaffController extends Controller
 {
-    //
+    public function index ()
+    {
+        $users = User::where('admin_status', false)->get();
+
+        return view('admin.staff', compact('users'));
+    }
+    
 }
---
+++ src/resources/views/admin/staff.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('title','スタッフ一覧')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/staff.css')  }}">
+@endsection
+
+@section('content')
+<div class="staff__content">
+  <div class="staff__inner">
+    <h1 class="staff__title">勤怠詳細</h1>
+    <div class="staff__wrapper">
+      <table class="staff__table">
+        <tr class="staff__row">
+          <th class="staff__head">名前</th>
+          <th class="staff__head">メールアドレス</th>
+          <th class="staff__head">月次勤怠</th>
+        </tr>
+
+        @foreach ($users as $user) 
+        <tr class="staff__row">
+          <td class="staff__data">
+            {{ $user->name }}
+          </td>
+          <td class="staff__data">
+            {{ $user->email }}
+          </td>
+          <td class="staff__data">
+            <a class="staff__detail" href="/attendance/staff/{{ $user->id }}">詳細</a>
+          </td>
+        </tr>
+        @endforeach
+      </table>
+    </div>
+  </div>
+</div>
+@endsection
